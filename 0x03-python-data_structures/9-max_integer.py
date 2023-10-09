c660e4838be2e0d9e60a2026a309761ff6e4006d
ukauche <uchekaluka@gmail.com>
2023-10-09 16:03:46 +0000

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == []:
+        return None
+    maximum = my_list[0]
+    for x in range(len(my_list)):
+        if my_list[x] > maximum:
+            maximum = my_list[x]
+    return maximum
